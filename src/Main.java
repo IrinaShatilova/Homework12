17a9bf803dece322c279941fb590997dc629ded1
shati <shatilova_ira@mail.ru>
2022-12-16 23:08:49 +0600

    ДЗ по теме объекты и классы выполнено

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Homework 12");
-        Author levTolstoy = new Author("Лев","Толстой");
+        Author levTolstoy = new Author("Лев"," Толстой");
         Book warAndWorld = new Book("Война и мир",levTolstoy,1980);
-        Author aleksandrPushkin = new Author("Александр","Пушкин");
+        Author aleksandrPushkin = new Author("Александр"," Пушкин");
         Book evgeniyOnegin = new Book("Евгений Онегин", aleksandrPushkin,1979);
         warAndWorld.setYear(2022);
         System.out.println("Книга 1 - " + warAndWorld.getName() + ", автор: " + levTolstoy.getFirstName() + levTolstoy.getLastName() + ", год издания: " + warAndWorld.getYear());
